0257473982ffcc0ba634469ba00a47456ebfee2b
thinker227 <thinker0227@gmail.com>
2021-12-08 22:52:05 +0100

    Added project files

---
+++ src/BridgesOfKaliningrad/Bridges.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BridgesOfKaliningrad;
+
+public static class Bridges {
+
+	public static bool BridgesAreValid(string layout)
+	{
+		throw new NotImplementedException();
+	}
+
+}
---
+++ src/BridgesOfKaliningrad/BridgesTests.cs
@@ -0,0 +1,85 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BridgesOfKaliningrad {
+
+[TestClass]
+public class BridgesTests
+{
+
+	[TestMethod]
+	public void Test()
+	{
+		Assert.IsTrue(Bridges.BridgesAreValid(
+			"#-#"
+		));
+
+		Assert.IsTrue(Bridges.BridgesAreValid(
+			"#-#\n" +
+			"|\n" +
+			"#"
+		));
+
+		Assert.IsFalse(Bridges.BridgesAreValid(
+			"#\n" +
+			"|\n" +
+			"#-#\n" +
+			"|\n" +
+			"#"
+		));
+
+		Assert.IsTrue(Bridges.BridgesAreValid(
+			"######\n" +
+			"| ### \n" +
+			"|   | \n" +
+			"##  ##\n"
+		));
+
+		Assert.IsTrue(Bridges.BridgesAreValid(
+			"####  #\n" +
+			" ##--##\n" +
+			" |   # \n" +
+			"##   ##\n" +
+			" ###-##\n"
+		));
+
+		Assert.IsTrue(Bridges.BridgesAreValid(
+			"###  \n" +
+			" ### \n" +
+			" | | \n" +
+			"##-# \n" +
+			"|  ##\n" +
+			"##  #\n"
+		));
+
+		Assert.IsFalse(Bridges.BridgesAreValid(
+			"##########\n" +
+			" |  |   | \n" +
+			"###### ###\n" +
+			"######-###\n" +
+			" |  |   | \n" +
+			"##########"
+		));
+
+		Assert.IsFalse(Bridges.BridgesAreValid(
+			"#####         ####\n" +
+			" #########--### | \n" +
+			"    ####    |   | \n" +
+			"#     |     #   ##\n" +
+			"#     |     ##  ##\n" +
+			"##----###      ###\n" +
+			" ##  #######---## \n"
+		));
+
+		Assert.IsFalse(Bridges.BridgesAreValid(
+			"#####         ####\n" +
+			" #########--### | \n" +
+			"    ####    |   | \n" +
+			"#     |     #   ##\n" +
+			"#     |     ##--##\n" +
+			"##----###      ###\n" +
+			" ##  #######---## \n"
+		));
+	}
+
+}
+}
